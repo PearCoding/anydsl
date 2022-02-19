5f53a75fda60842c1f623cffa7daaaf77d7c719a
PearCoding <pearcoding@gmail.com>
2022-02-19 13:35:00 +0100

    Improved docker for cuda

---
+++ docker/setup_newest_cmake.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt-get install -y apt-transport-https ca-certificates gnupg software-properties-common wget
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+
+# Install will be done later
