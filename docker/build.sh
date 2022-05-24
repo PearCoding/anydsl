4322b75ab3bb4d8801220c7a026db5011dfe6ae8
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-05-24 13:35:44 +0200

    New versions

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,4 @@ DOCKER_USER=pearcoding
 cd $SCRIPT_DIR
 # Build all docker image flavours
 docker build -t $DOCKER_USER/anydsl:latest-avx2-jit -f Dockerfile-AVX2-JIT ..
-docker build -t $DOCKER_USER/anydsl:latest-cuda-jit -f Dockerfile-CUDA-JIT ..
+# docker build -t $DOCKER_USER/anydsl:latest-cuda-jit -f Dockerfile-CUDA-JIT ..
